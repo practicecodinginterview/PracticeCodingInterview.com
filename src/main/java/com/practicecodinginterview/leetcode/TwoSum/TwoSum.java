9ceb93a0d851ff27524e43f96f3e5be080efa42d
PracticeCodingInterview.com <info@PracticeCodingInterview.com>
2019-06-05 06:36:07 -0700

    Add naive solution to TwoSum
    The TwoSum problem has an O(n^2) solution where you look at every
    combination of pairs using a double for-loop. Included for clarity.

--- src/main/java/com/practicecodinginterview/leetcode/TwoSum/TwoSum.java
+++ src/main/java/com/practicecodinginterview/leetcode/TwoSum/TwoSum.java
@@ -41,4 +41,22 @@ public class TwoSum {
 
 		return results;
 	}
+
+	/**
+	 * The naive O(n^2) solution.
+	 */
+	private int[] twoSumNaive(int[] nums, int target) {
+		int[] results = new int[2];
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					results[0] = i;
+					results[1] = j;
+				}
+			}
+		}
+
+		return results;
+	}
 }
